16f044ec5c8710c2c022c454a8ceb7b7ab0081c9
xXMaddyX <maddyaion@hotmail.de>
2026-07-18 18:56:08 +0200

    update PiepsTron

---
+++ BackendServer/PTBackend.cs
@@ -0,0 +1,59 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace PTBackend;
+
+public class BTBackendHandler{
+    public WebApplication? PiepsTronBackend { get; set; } = null;
+    public string Url { get; private set; } = "http://localhost:3030";
+    private string _defaultAllowSpecsCors { get; } = "_myAllowSpecsCors";
+    private string? AllowSpecsCors { get; set; }
+//###############################################################################################################
+    public WebApplication InitBackend(string? url ,WebApplicationOptions? webOptions, Action<CorsOptions>? setupAction, string? corsPolicyName = null) {
+        if (webOptions != null && setupAction != null && url != null) {
+            Url = url;
+            AllowSpecsCors = corsPolicyName;
+            SetUserOptions(webOptions, setupAction);
+        } else { SetBasicOptions(); };
+        StartServer(Url);
+        return PiepsTronBackend!;
+    }
+//###############################################################################################################
+    private void StartServer(string url) {
+        if (PiepsTronBackend != null) {
+            Task.Run(() => {
+                PiepsTronBackend.Run(url);
+            });
+        } else {
+            Console.WriteLine("ERROR AT Starting App Backend!!!");
+        }
+    }
+//###############################################################################################################
+    private void SetBasicOptions() {
+        var AppOptions = new WebApplicationOptions{
+                WebRootPath = Path.Combine(AppContext.BaseDirectory, "src"),
+        };
+        var Builder = WebApplication.CreateBuilder(AppOptions);
+        Builder.Services.AddCors(options => {
+            options.AddPolicy(_defaultAllowSpecsCors, policy => {
+                    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+        });
+        BuildServer(Builder);
+    }
+//###############################################################################################################
+    private void SetUserOptions(WebApplicationOptions webOptions, Action<CorsOptions> setupAction) {
+        var Builder = WebApplication.CreateBuilder(webOptions);
+            Builder.Services.AddCors(setupAction);
+        BuildServer(Builder);
+    }
+//###############################################################################################################
+    private void BuildServer(WebApplicationBuilder Builder) {
+        PiepsTronBackend = Builder.Build();
+        PiepsTronBackend.UseStaticFiles();
+        if (AllowSpecsCors != null) { PiepsTronBackend.UseCors(AllowSpecsCors); }
+        else { PiepsTronBackend.UseCors(_defaultAllowSpecsCors); }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,46 +1,39 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using PTBackend;
 
 Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
 Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
 
-
+//###############################################################################################################
+//------------------------------------------>>>>INIT_PIEPS_BACKEND<<<<------------------------------------------#
+//###############################################################################################################
 var MyAllowSpecsCors = "_myAllowSpecsCors";
 var options = new WebApplicationOptions{
     Args = args,
     WebRootPath = Path.Combine(AppContext.BaseDirectory, "src"),
 };
-
-var Builder = WebApplication.CreateBuilder(options);
-Builder.Services.AddCors(options => {
-    options.AddPolicy(MyAllowSpecsCors, policy => {
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+//###############################################################################################################
+var BTBackend = new BTBackendHandler();
+var App = BTBackend.InitBackend("http://localhost:4040", options, setup => {
+    setup.AddPolicy(MyAllowSpecsCors, poly => {
+        poly.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
     });
 });
-
-var App = Builder.Build();
-App.UseStaticFiles();
-App.UseCors(MyAllowSpecsCors);
-App.Urls.Add("http://localhost:4040");
-
+//###############################################################################################################
 App.MapGet("/", async ctx => {
     try {
-        using var openFile = File.Open(Path.Combine(AppContext.BaseDirectory, "src/index.html"), mode: FileMode.Open, access: FileAccess.Read);
-        var buffer = new byte[openFile.Length];
-        await openFile.ReadExactlyAsync(buffer);
-        await ctx.Response.WriteAsync(System.Text.Encoding.UTF8.GetString(buffer));
+        var fileData = await File.ReadAllTextAsync(Path.Combine(AppContext.BaseDirectory, "src/index.html"));
+        await ctx.Response.WriteAsync(fileData);
     } catch (Exception ex) {
         Console.WriteLine(ex);
         await ctx.Response.WriteAsync("ERROR 500");
     }
 });
-
-void RunServer() {
-    Task.Run(() => {App.Run();});
-}
-RunServer();
+//###############################################################################################################
+//----------------------------------------->>>>INIT_WINDOW_APPLICATION<<<<--------------------------------------#
+//###############################################################################################################
 
 var WindowApplication = new System.Windows.Application();
 var window = new System.Windows.Window{Height = 800, Width = 1440, Name = "PiepsTron"};
@@ -51,7 +44,7 @@ window.Closing += (sender, e) => {
     App.StopAsync();
 };
 window.Loaded += async (sender, e) => {
-    Webview.Source = new Uri("http://localhost:4040");
+    Webview.Source = new Uri(BTBackend.Url);
     await Webview.EnsureCoreWebView2Async();
 };
 
